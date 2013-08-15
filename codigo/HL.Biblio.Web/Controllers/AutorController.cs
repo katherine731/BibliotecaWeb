e15c8500857e65d467f7d4111ecfbfc4cf031da6
katherine731 <kitty.helen.rp@gmail.com>
2013-08-15 10:51:36 -0400

    se implemento el caso de uso editorial con todos los requerimientos establecidos

---
+++ codigo/HL.Biblio.Web/Controllers/AutorController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HL.Biblio.POCO;
+using HL.Biblio.BLL;
+
+namespace HL.Biblio.Web.Controllers {
+    public class AutorController : Controller {
+        //
+        // GET: /Autor/
+
+        public ActionResult Index() {
+            try {
+                return View();
+            } catch(Exception ex) {
+                return View("~/Views/Shared/Error.cshtml", new Models.ManejadorError(ex));
+            }
+        }
+
+        public ActionResult Lista() {
+            try {
+                return PartialView(AutorBLL.List2());
+            } catch(Exception ex) {
+                return View("~/Views/Shared/ErrorSub.cshtml", new Models.ManejadorError(ex));
+            }
+        }
+
+        //
+        // GET: /Autor/Details/5
+
+        public ActionResult Details(int id) {
+            try {
+                Autor a = AutorBLL.Get(id);
+                if(a != null)
+                    return View(a);
+                else
+                    return View("~/Views/Shared/Error.cshtml", new Models.ManejadorError("Nulo", "", "", "", ""));
+            } catch(Exception ex) {
+                return View("~/Views/Shared/Error.cshtml", new Models.ManejadorError(ex));
+            }
+        }
+
+        //
+        // GET: /Autor/Create
+
+        public ActionResult Create() {
+            ViewBag.Paises = BLL.PaisBLL.ListActivos();
+            return View();
+        }
+
+        //
+        // POST: /Autor/Create
+
+        [HttpPost]
+        public ActionResult Create(Autor autor) {
+            try {
+                AutorBLL.Create(autor);
+                return RedirectToAction("Index");
+            } catch(Excepcion ec) {
+                ViewBag.mensaje = new Models.Mensaje(ec.CreateMensaje(), Models.Mensaje.TipoMsg.error);
+                return View("Create", autor);
+            } catch(Exception ex) {
+                return View("~/Views/Shared/Error.cshtml", new Models.ManejadorError(ex));
+            }
+        }
+
+        //
+        // GET: /Autor/Edit/5
+
+        public ActionResult Edit(int id) {
+            try {
+                ViewBag.Paises = BLL.PaisBLL.ListActivos();
+                Autor a = AutorBLL.Get(id);
+                if(a != null && ViewBag.Paises != null)
+                    return View(a);
+                else
+                    return View("~/Views/Shared/Error.cshtml", new Models.ManejadorError("Nulo", "", "", "", ""));
+            } catch(Exception ex) {
+                return View("~/Views/Shared/Error.cshtml", new Models.ManejadorError(ex));
+            }
+        }
+
+        //
+        // POST: /Autor/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, Autor autor) {
+            try {
+                autor.Id = id;
+                AutorBLL.Update(autor);
+                return RedirectToAction("Index");
+            } catch(Excepcion ec) {
+                ViewBag.mensaje = new Models.Mensaje(ec.UpdateMensaje(), Models.Mensaje.TipoMsg.error);
+                ViewBag.Paises = BLL.PaisBLL.ListActivos();
+                return View("Edit", autor);
+            } catch(Exception ex) {
+                return View("~/Views/Shared/Error.cshtml", new Models.ManejadorError(ex));
+            }
+        }
+
+        //
+        // POST: /Autor/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id) {
+            try {
+                BLL.AutorBLL.Delete(id);
+                return View("Lista", BLL.AutorBLL.List2());
+            } catch(Excepcion ec) {
+                ViewBag.mensaje = new Models.Mensaje(ec.DeleteMensaje(), Models.Mensaje.TipoMsg.error);
+                return View("Lista", BLL.AutorBLL.List2());
+            } catch(Exception ex) {
+                return View("~/Views/Shared/ErrorSub.cshtml", new Models.ManejadorError(ex));
+            }
+        }
+
+        [HttpPost]
+        public ActionResult CambiarEstado(int AutorId) {
+            try {
+                ViewBag.estado = BLL.AutorBLL.CambiarEstado(AutorId);
+                return View("~/Views/Shared/Estado.cshtml");
+            } catch {
+                ViewBag.estado = -1;
+                return View("~/Views/Shared/Estado.cshtml");
+            }
+        }
+    }
+}
---
+++ codigo/HL.Biblio.Web/Controllers/EditorialController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HL.Biblio.BLL;
+using HL.Biblio.POCO;
+
+namespace HL.Biblio.Web.Controllers {
+    public class EditorialController : Controller {
+        //
+        // GET: /Editorial/
+
+        public ActionResult Index() {
+            try {
+                return View();
+            } catch(Exception ex) {
+                return View("~/Views/Shared/Error.cshtml", new Models.ManejadorError(ex));
+            }
+        }
+
+        public ActionResult Lista() {
+            try {
+                return PartialView(EditorialBLL.List2());
+            } catch {
+                return View("~/Views/Shared/Error.cshtml");
+            }
+        }
+
+        //
+        // GET: /Editorial/Details/5
+
+        public ActionResult Details(int id) {
+            try {
+                Editorial e = EditorialBLL.Get(id);
+                if(e != null)
+                    return View(EditorialBLL.Get(id));
+                else
+                    return View("~/Views/Shared/Error.cshtml", new Models.ManejadorError("Nulo", "", "", "", ""));
+            } catch(Exception ex) {
+                return View("~/Views/Shared/Error.cshtml", new Models.ManejadorError(ex));
+            }
+        }
+
+        //
+        // GET: /Editorial/Create
+
+        public ActionResult Create() {
+            ViewBag.Paises = BLL.PaisBLL.ListActivos();
+            return View();
+        }
+
+        //
+        // POST: /Editorial/Create
+
+        [HttpPost]
+        public ActionResult Create(Editorial editorial) {
+            try {
+                EditorialBLL.Create(editorial);
+                return RedirectToAction("Index");
+            } catch(Exception ex) {
+                return View("~/Views/Shared/Error.cshtml", new Models.ManejadorError(ex));
+            }
+        }
+
+        //
+        // GET: /Editorial/Edit/5
+
+        public ActionResult Edit(int id) {
+            try {
+                ViewBag.Paises = BLL.PaisBLL.ListActivos();
+                Editorial e = EditorialBLL.Get(id);
+                if(e != null && ViewBag.Paises != null)
+                    return View(e);
+                else
+                    return View("~/Views/Shared/Error.cshtml", new Models.ManejadorError("Nulo", "", "", "", ""));
+            } catch(Exception ex) {
+                return View("~/Views/Shared/Error.cshtml", new Models.ManejadorError(ex));
+            }
+        }
+
+        //
+        // POST: /Editorial/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, Editorial editorial) {
+            try {
+                editorial.Id = id;
+                EditorialBLL.Update(editorial);
+                return RedirectToAction("Index");
+            } catch(Exception ex) {
+                return View("~/Views/Shared/Error.cshtml", new Models.ManejadorError(ex));
+            }
+        }
+
+        //
+        // POST: /Editorial/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id) {
+            try {
+                if(!BLL.EditorialBLL.Delete(id))
+                    ViewBag.mensaje = "No se pudo eliminar el registro, tiene participacion en alguna transaccion.";
+                return View("Lista", BLL.EditorialBLL.List2());
+            } catch(Exception ex) {
+                return View("~/Views/Shared/ErrorSub.cshtml", new Models.ManejadorError(ex));
+            }
+        }
+
+        [HttpPost]
+        public ActionResult CambiarEstado(int EditorialID) {
+            try {
+                ViewBag.estado = BLL.EditorialBLL.CambiarEstado(EditorialID);
+                return View("~/Views/Shared/Estado.cshtml");
+            } catch(Exception ex) {
+                return View("~/Views/Shared/Error.cshtml", new Models.ManejadorError(ex));
+            }
+        }
+    }
+}
